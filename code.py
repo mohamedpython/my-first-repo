4484e180ca140bdf96ccb41a4990a41713964d5a
mohamed AbdelRahman <m.abd@gmail.com>
2019-03-11 14:16:02 +0200

    Initial commit

---
+++ code.py
@@ -0,0 +1,9 @@
+
+def test(x,y):
+    all_Leafs=range(1,10)
+    for devices in all_Leafs:
+     if Y < 3:  # HERE WE WILL SAME ANYCAST IP FOR EACH SUCCESSIVE LEAFS
+         Y = Y + 1
+     else:
+         x=x +1
+        
